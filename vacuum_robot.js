a38eeba6b3631b7e225cbc64d267db099f13ade9
Jae Park <jae.park510@gmail.com>
2019-09-03 02:14:25 -0700

    Changed data structure of room from nested arrays to string

--- vacuum_robot.js
+++ vacuum_robot.js
@@ -8,7 +8,7 @@ let directions = [];
 let roomDimensions = [];
 let dirtLocations = [];
 let robotPosition = [];
-let room = [];
+let room = '';
 let numOfDirtMoundsCleaned = 0;
 
 // Parses text file to extract room data
@@ -20,7 +20,6 @@ const textFileReader = readline.createInterface({
         { data.push(line) }
         )
         .on('close', () => {
-          console.log("BEFORE CLEANING");  
           splitData(data);
           makeMoves(directions);
           showResults();
@@ -41,70 +40,70 @@ const splitData = (txt) =>{
     if(data.length > 3){
         dirtLocations = data.slice(2, data.length-1);
     }
+    
+    showData();
 
+    console.log("\nBEFORE CLEANING");  
     buildRoom();
+}
+
+const showData = () => {
+    console.log("\n------------ DATA GATHERED ------------");
+    console.log(`Room Dimensions: ${roomDimensions[0]}x${roomDimensions[1]}`);
+    console.log(`Robot Starting Position: ${robotPosition}`);
+    console.log(`Dirt Locations: ${dirtLocations}`);
+    console.log("---------------------------------------");
 }
 
 // Fills room array based on dimensions of input
 const buildRoom = () => {
     // Legend for grid values
-    console.log("[ D - dirt mound, R - robot location, x - empty space ]");
+    console.log("[ (D) - dirt mound | (R) - robot location | (.) - empty space ]");
 
     for(let i = 0; i < roomDimensions[1]; i++){
-        let gridRow = [];
+        let gridRow = '';
         for(let j = 0; j <= roomDimensions[0]; j++){
             // Simulate a y-axis by using the 0th index of each row
             if(j === 0){
-                gridRow.push(` ${ roomDimensions[1] - i } `);
-            } else {
-                gridRow.push(' x ');
+                if(roomDimensions[1] - i < 10){
+                    gridRow+=(` ${ roomDimensions[1] - i } `);
+                }
+                 if(roomDimensions[1] - i >= 10){
+                    gridRow+=(`${ roomDimensions[1] - i } `);
+                }
+                
+            } else if (dirtLocations.includes(`${j} ${roomDimensions[1]- i}`)) {
+                gridRow+='(D)';
+            } else if (robotPosition[0] === parseInt(j) && robotPosition[1] === roomDimensions[1] - parseInt(i)){
+                gridRow+='(R)';
+            }
+            else {
+                gridRow+=' . ';
             }
         }
-        room.push(gridRow);
+        gridRow+= '\n';
+        room+=gridRow;
     }
 
-    // Last row of x-axis must be populated with numbers
-    let lastRow = [];
+    // Simulate x-axis with last row by filling with numbers unless dirt is there
+    let lastRow = '';
     for(let i = 0; i <= roomDimensions[0]; i++){
-        lastRow.push(` ${i} `);
-    }
-
-    room.push(lastRow);
-    populateRoom();
-}
-
-// Helper function to populate the dirt mounds and starting location of robot
-const populateRoom = () => {
-    let dirtCoordinates = [];
-
-    // Create an array of x,y coordinates from dirtLocations
-    dirtLocations.forEach((loc)=>{
-        dirtCoordinates.push(loc.split(' '));
-    })
-
-    // Convert the strings to numbers
-    for(let i = 0; i < dirtCoordinates.length; i++){
-        for(let j = 0; j < dirtCoordinates[i].length; j++){
-            dirtCoordinates[i][j] = parseInt(dirtCoordinates[i][j]);
+        if(dirtLocations.includes(`${i} 0`)){
+            lastRow+= `(D)`;
+        } else {
+            if(i < 10){
+              lastRow+=` ${i} `;
+            } else if(i >= 10){
+              lastRow+=`${i} `; 
+            }
+           
         }
     }
 
-    // Place the dirt mounds in the room array
-    for(let i = 0; i < dirtCoordinates.length; i++){
-       let row = roomDimensions[1] - dirtCoordinates[i][1];
-       let col = dirtCoordinates[i][0];
-       // Mark dirt with D
-       room[row][col] = ' D ';
-    }
-
-    // Place the robot in the room array
-    let row = roomDimensions[1] - robotPosition[1];
-    let col = robotPosition[0];
-    // Mark starting location of robot with R
-    room[row][col] = ' R ';
-
+    room+=lastRow;
     console.log(room);
 }
+
 
 // Move the robot based on the directions data
 const makeMoves = (directions) => {
@@ -153,8 +152,8 @@ const cleanIfDirty = (robotPosition) => {
 // Show final results after cleaning the room
 const showResults = () => {
     room = [];
-    console.log("AFTER CLEANING");
+    console.log("\nAFTER CLEANING");
     buildRoom();
-    console.log(`Final Robot Position: ${robotPosition[0]}, ${robotPosition[1]}`);
+    console.log(`\nFinal Robot Position: ${robotPosition[0]}, ${robotPosition[1]}`);
     console.log("Number of Dirt Mounds Cleaned: ", numOfDirtMoundsCleaned);
 }
